8b9df0a206ff4a8aadce6b6977823d5f96cba030
harshau007 <amanupadhyay2004@gmail.com>
2024-01-05 19:20:26 +0530

    Signout

---
+++ app/signout/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import  React, { useState } from "react";
+import Navbar from "../components/Navbar";
+
+const Page: React.FC = () => {
+
+  return (
+    <>
+    <Navbar/>
+        Sign Out
+    </>
+  );
+};
+export default Page;
