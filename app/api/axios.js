b85d53b760a77e6c3dc48b00514b9e2b5770b680
harshau007 <amanupadhyay2004@gmail.com>
2024-01-07 16:21:00 +0530

    URL for axios

--- app/api/axios.js
+++ app/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:4000" || "https://house-backend-393e.onrender.com";
+const URL = "https://house-backend-393e.onrender.com";
 export default axios.create({
   baseURL: URL,
 });
